88f445d85246b7f3cd54810e7f1d4af070f45db2
faiz-x-it <70457642+faiz-x-it@users.noreply.github.com>
2022-06-17 21:21:52 +0700

    Update script.js

--- app/script.js
+++ app/script.js
@@ -3,18 +3,19 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(windowWidth, windowHeight);
+    canvas = createCanvas(windowWidth, windowHeight-30);
     video = createCapture(VIDEO);
     video.hide();
-	  colorPicker = createColorPicker('#ffffff');
-  colorPicker.position(0, 20);
+	colorPicker = createColorPicker('#ffffff');
+	colorPicker.position(0, 20);
 	fil = "";
 	button = createButton('snapshot');
-  button.position(50/100*windowWidth, windowHeight);  button.mousePressed(take_snapshot);
+	button.position(50/100*windowWidth, windowHeight-50);  
+	button.mousePressed(take_snapshot);
 }
 
 function draw(){
-    image(video, 0, 0, windowWidth, windowHeight);
+    image(video, 0, 0, windowWidth, windowHeight-30);
 	   tint(colorPicker.color());
 	fil = document.getElementById("filter").value;
 	if(fil==="THRESHOLD"){filter(THRESHOLD);}
@@ -23,7 +24,7 @@ function draw(){
 	if(fil==="INVERT"){filter(INVERT);}
 	if(fil==="POSTERIZE"){filter(POSTERIZE, 3);}
 	if(fil==="DILATE"){filter(DILATE);}
-	if(fil==="BLUR"){filter(BLUR);}
+	if(fil==="BLUR"){filter(BLUR, 5);}
 	if(fil==="ERODE"){filter(ERODE);}
 }
 
