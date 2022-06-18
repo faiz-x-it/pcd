0deb2b2430b505070171aaf0d07395e6fac02ed0
faiz-x-it <70457642+faiz-x-it@users.noreply.github.com>
2022-06-18 13:07:37 +0700

    Create pc.js

---
+++ file/pc.js
@@ -0,0 +1,36 @@
+function preload(){
+    gambar = loadImage('aq.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(windowWidth, windowHeight-30);
+    
+	colorPicker = createColorPicker('#ffffff');
+	colorPicker.position(50, windowHeight-30);
+	fil = "";
+	button = createButton('foto');
+	button.size(50, 26);  
+	button.position(0, windowHeight-30); 
+	button.mousePressed(take_snapshot);
+}
+
+function draw(){
+    image(gambar, 0, 0, windowWidth, windowHeight-30);
+	   tint(colorPicker.color());
+	fil = document.getElementById("filter").value;
+	if(fil==="THRESHOLD"){filter(THRESHOLD);}
+	if(fil==="GRAY"){filter(GRAY);}
+	if(fil==="OPAQUE"){filter(OPAQUE);}
+	if(fil==="INVERT"){filter(INVERT);}
+	if(fil==="POSTERIZE"){filter(POSTERIZE, 3);}
+	if(fil==="DILATE"){filter(DILATE);}
+	if(fil==="BLUR"){filter(BLUR, 5);}
+	if(fil==="ERODE"){filter(ERODE);}
+}
+
+function take_snapshot(){
+    save('test.png');
+}
+function copy(){
+    copy(image)
+}
